96b0c6021a2eef88bbfca4491c13f1c6aad07487
Stanislav Bashkyrtsev <stanislav.bashkirtsev@gmail.com>
2021-12-04 21:24:50 +0300

    Fix trig calculations
    TODO: translate points before and after rotation to define a center of
    rotation

--- firstJS.js
+++ firstJS.js
@@ -41,12 +41,17 @@ function newDrawSecond() {
 function newDrawRotation() {
     let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
     let newImgData = ctx.createImageData(canvas.width, canvas.height);
-    let dNew = new Drawing(ctx, canvas.width, canvas.height, newImgData );
-    let dOld = new Drawing(ctx, canvas.width, canvas.height, imgData );
+    let dNew = new Drawing(ctx, canvas.width, canvas.height, newImgData);
+    let dOld = new Drawing(ctx, canvas.width, canvas.height, imgData);
+    const center = {x: 75, y: 75};
     for (let x=0; x<canvas.width; x++) {
         for (let y=0; y<canvas.height; y++) {
-            let angle = Math.PI/18;
-            dNew.setAlpha(x*Math.cos(angle) - y*Math.sin(angle), y*Math.sin(angle) + x*Math.cos(angle), dOld.getAlpha(x, y));
+            let oldCoord = {x: x - center.x, y: center.y - y};
+            const angle = Math.PI/18;
+            let alpha = dOld.getAlpha(oldCoord.x, oldCoord.y);
+            let newX = Math.round(oldCoord.x*Math.cos(angle) - oldCoord.y*Math.sin(angle)) - center.x;
+            let newY = center.y - Math.round(oldCoord.x*Math.sin(angle) + oldCoord.y*Math.cos(angle));
+            dNew.setAlpha(newX, newY, alpha);
         }
     }
     dNew.draw();
@@ -55,20 +60,11 @@ function newDrawRotation() {
 //x' = y*Math.sin(10) + x*Math.cos(10)
 function drawZigzag() {
     for(let i=0; i<10; i++) {
-        if(i%2==0) {
-            for(let s=0; s<35; s++) {
-                setTimeout(()=>{
-                 newDrawFirst();
-                 newDrawRotation();
-                }, 20);
-            }
-        } else {
-            for(let s=0; s<35; s++) {
-                setTimeout(()=>{
-                 newDrawFirst();
-                 newDrawRotation();
-                }, 20);
-            }
+        for (let s = 0; s < 35; s++) {
+            setTimeout(() => {
+                // newDrawFirst();
+                newDrawRotation();
+            }, 20);
         }
     }
 }
