15cd66fac200aa41a6e8a695da13a676b8191e2b
Админ <anutapontera@gmail.com>
2021-12-26 19:15:36 +0200

    hello4

--- Drawing.js
+++ Drawing.js
@@ -1,18 +1,32 @@
 'use strict'
 
 class Drawing {
-    constructor(points) {
+    constructor(points, x, y) {
         this.points = points;
+        this.x = x;
+        this.y = y;
     }
 
-    transform (f) {
-        const newPoints = [];
-        for (let i=0; i< this.points.length; i++) {
-            let newCoord = f(this.points[i].x, this.points[i].y);
-            newPoints.push({y: newCoord.y , x: newCoord.x});
-        }
-        return new Drawing(newPoints)
+    translate (f) {
+        let newCoord = f(this.x, this.y);
+        return new Drawing(this.points, newCoord.x, newCoord.y);
     }
+
+    transform (f, ifMovement) {
+        if (!ifMovement) {
+            const newPoints = [];
+            for (let i=0; i< this.points.length; i++) {
+                let newCoord = f(this.points[i].x, this.points[i].y);
+                newPoints.push({y: newCoord.y , x: newCoord.x});
+            }
+            return new Drawing(newPoints, this.x, this.y);
+        } else {
+            return translate(f);
+        }
+    }
+
+
+
     toCanvasPixels(canvasWidth) {
         const pixels = [];
         for (let point of this.points) {
@@ -20,9 +34,6 @@ class Drawing {
             const y = Math.round(point.y);
             const pixelIdx = x + y * canvasWidth;
             const pixelStart = pixelIdx*4;
-            // pixels[pixelStart]   = 0;
-            // pixels[pixelStart+1] = 0;
-            // pixels[pixelStart+2] = 0;
             pixels[pixelStart+3] = 255;
         }
         return pixels;
--- src/test/js/DrawingTest.js
+++ src/test/js/DrawingTest.js
@@ -2,10 +2,66 @@ const Drawing = require('../../../Drawing');
 const assert = require('assert');
 
 describe('Drawing', ()=> {
-    it('transforms pixels according to specified function', () => {
-        const d = new Drawing([]);
+    it('checking array length', () => {
+        const d = new Drawing([], 0, 0);
         assert.strictEqual(d.points.length, 0);
     });
+
+    it('checking array length 2', () => {
+        const d = new Drawing([{x: 5, y: 7}, {x: 9, y: 3}], 0, 0);
+        assert.strictEqual(d.points.length, 2);
+    });
+
+    it('checking value of element', () => {
+        const d = new Drawing([{x: 5, y: 7}, {x: 9, y: 3}], 0, 0);
+        assert.strictEqual(d.points[0].x, 5);
+    });
+
+    it('transform with movement-function', () => {
+        let d = new Drawing([{x: 1, y: 2}, {x: 2, y: 2}], 1, 2);
+        d = d.transform((x, y) => {
+            return {x: x+1, y: y+2}
+        }, true);
+        assert.strictEqual(d.x, 2);
+        assert.strictEqual(d.y, 4);
+    });
+
+    it('translate does not change points', () => {
+        let d = new Drawing([{x: 1, y: 2}, {x: 2, y: 2}], 1, 2);
+        let sign = -1;
+        d = d.transform((x, y) => {
+            return {x: x+1, y: y+2*sign}
+        }, true);
+        assert.strictEqual(d.points[1].x, 2);
+    });
+
+    it('transform with rotation-function', () => {
+        let d = new Drawing([{x: 75, y: 75}, {x: 76, y: 75}], 3, 5);
+        d = d.transform(rotate);
+        assert.strictEqual(d.points[0].x.toFixed(2), '75.00');
+    });
+
+    it('transform with rotation-function 2', () => {
+        let d = new Drawing([{x: 75, y: 75}, {x: 76, y: 75}], 3, 5);
+        d = d.transform(rotate);
+        assert.strictEqual(d.points[0].y.toFixed(2), '75.00');
+    });
+
+    it('transform with rotation-function 3', () => {
+        let d = new Drawing([{x: 0, y: 1}, {x: 1, y: 1}], 3, 5);
+        d = d.transform(rotate);
+        assert.strictEqual(d.x, 3);
+    });
+
 });
+
+let center = {x: 75, y: 75};
+function rotate(x, y) {
+     let oldCoord = {x: x - center.x, y: center.y - y};
+     const angle = Math.PI/36;
+     let newX = center.x + oldCoord.x*Math.cos(angle) - oldCoord.y*Math.sin(angle);
+     let newY = -(-center.y + oldCoord.x*Math.sin(angle) + oldCoord.y*Math.cos(angle));
+     return {x: newX, y: newY};
+}
 
 
